74d1f178579b8c51c0b0822ab563570af70c562a
mhj212 <mhj212@gmail.com>
2014-01-27 15:17:43 -0500

    Matt J's commit

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -15,9 +15,20 @@ class MissionsController < ApplicationController
   end
 
   def update
+    id = params[:id]
+    @update_m = Mission.find(id)
+    @update_m.title = params[:new_title]
+    @update_m.target = params[:new_target]
+    @update_m.launch_date = params[:new_launch_date]
+    @update_m.vehicle_name = params[:new_vehicle_name]
+    @update_m.save!
+    redirect_to :back
   end
 
   def edit
+    id = params[:id]
+    @mission = Mission.find(id)
+    redirect_to :back
   end
 
  end
