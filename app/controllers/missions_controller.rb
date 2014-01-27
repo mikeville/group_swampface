5b5c7fafc24e1baac793182b049ff122fec9445c
brsoff <brsoff@gmail.com>
2014-01-27 15:08:59 -0500

    fixing controller file

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -14,12 +14,8 @@ class MissionsController < ApplicationController
   end
 
   def destroy
-<<<<<<< HEAD
     Mission.delete(params[:id])
     redirect_to '/'
-=======
-    # Brendan
->>>>>>> 908c1ce3a1d92b55b83be6a4aa2ea496126591bd
   end
 
   def update
