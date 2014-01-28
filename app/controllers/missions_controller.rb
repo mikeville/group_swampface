793efcf1ec2143b1cb996b25f8694082c689d1d2
brsoff <brsoff@gmail.com>
2014-01-28 13:58:00 -0500

    cleaned up controllers

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -22,28 +22,14 @@ class MissionsController < ApplicationController
   end
 
   def update
-<<<<<<< HEAD
     @mission = Mission.find(params[:id])
     @mission.update_attributes(params[:mission])
-
     redirect_to root_path
-=======
-    id = params[:id]
-    @update_m = Mission.find(id)
-    @update_m.title = params[:new_title]
-    @update_m.target = params[:new_target]
-    @update_m.launch_date = params[:new_launch_date]
-    @update_m.vehicle_name = params[:new_vehicle_name]
-    @update_m.save!
-    redirect_to :back
   end
 
   def edit
-    id = params[:id]
-    @mission = Mission.find(id)
-    redirect_to :back
-    # Matt
->>>>>>> 89ccaee9563b8b2d6b4ce74d3af868fe748c6406
+    @mission = Mission.find(params[:id])
+    redirect_to root_path
   end
 
   def edit
